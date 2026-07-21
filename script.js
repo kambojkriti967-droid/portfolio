5ce9a9c3f8edbc8c9743a6f7460259f83f6a8f19
kambojkriti967-droid <kambojkriti967@gmail.com>
2026-07-21 19:27:27 +0530

    Refactor typing animation and theme toggle functionality

--- script.js
+++ script.js
@@ -222,18 +222,3 @@ topBtn.addEventListener("click", () => {
 });
 
 
-// ===============================
-// Contact Form
-// ===============================
-
-const form = document.querySelector("form");
-
-form.addEventListener("submit", function (e) {
-
-    e.preventDefault();
-
-    alert("Thank you! Your message has been received.");
-
-    form.reset();
-
-});
